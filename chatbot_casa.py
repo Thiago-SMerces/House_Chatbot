e613e0db1b0e498eac5e004d56c3d2b0e35878f9
Thiago-SMerces <thiago_merces@outlook.com>
2020-12-06 23:12:43 -0300

    Chatbot

--- chatbot_casa.py
+++ chatbot_casa.py
@@ -1,6 +1,21 @@
 from generated_tree import Tree
+
 t = Tree()
-continua = input("Continua? ")
-while continua != "0":
-    print(t.decision())
-    continua = input("Continua? ")
+continua = None
+erro = 0
+while continua != "2":
+    print("_____________________________________________________\n")
+    print("Chatbot de estimativa de preços ofertados por casas no CEP 98106 \n")
+    print("_____________________________________________________\n")
+    continua = input("Fazer estimativa? (1 para sim, 2 para não)\n")
+    if continua == "1":
+        print(t.decision())
+    elif continua == "2":
+        print("Encerrando programa. Até mais!")
+    else:
+        print("Comando inválido, tente novamente!")
+        erro +=1
+
+    if erro == 3:
+        print("Comandos inválidos sucessivos! Encerrando programa")
+        break
